8be4b8a520969abad3b95b24bd188b09cbebcdfc
synom <synom@10.232.161.139>
2024-08-29 12:15:59 -0500

    I wrote another message :)

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,5 +5,6 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
 		System.out.println("Hi!!! Omg I did itttt");
+		System.out.println("what's your favorite sport?");
 	}
 }
